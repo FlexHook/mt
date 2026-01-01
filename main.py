b77f8b25da39398b29a5f6564b86a565cc6c3c83
klaas8 <132535742+klaas8@users.noreply.github.com>
2026-01-01 15:08:45 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ Retry = {}
 accounts_list = {}
 s_list = {}
 
-prefs.put("ms", 666)
+prefs.put("ms", "66")
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
